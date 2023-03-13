173c07e187914ce36f4f620a8300f0c01ce0092b
John Park <park.hokyun@gmail.com>
2023-03-13 00:58:30 -0400

    transfer

---
+++ lab2_template_w3/adminsite/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Course, Instructor, Lesson
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+    inlines = [LessonInline]
+admin.site.register(Course, CourseAdmin)
+
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ['user', 'full_time']
+admin.site.register(Instructor, InstructorAdmin)
